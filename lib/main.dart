dc2d624b536bb0446db74571eb22d9200d1db40d
desya.kristianto <desya.tech@gmail.com>
2021-11-19 16:22:20 +0700

    add Search

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.white),
       home: MyHomePage(),
     );
   }
@@ -241,7 +241,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 : ListView.builder(
                     itemCount: _note.length,
                     itemBuilder: (context, index) => Card(
-                      color: Colors.white60,
+                      color: Colors.white,
                       // margin: const EdgeInsets.all(15),
                       child: ListTile(
                         title: Text(_note[index]['title']),
